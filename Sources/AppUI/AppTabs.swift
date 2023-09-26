1089347a6ff4ca775fe3e5ceff7c3ee78873d61f
marcprux <mwp1@cornell.edu>
2023-09-25 23:00:58 -0400

    Update project structure

--- Sources/AppUI/AppTabs.swift
+++ Sources/AppUI/AppTabs.swift
@@ -8,7 +8,7 @@ let logger = Logger(subsystem: "app.ui", category: "AppUI")
 /// The tabs for the app.
 enum AppTabs : String, CaseIterable {
     /// The initial default tab shown in the app
-    static let defaultTab = AppTabs.settings
+    static let defaultTab = AppTabs.home
 
     case home
     case content
